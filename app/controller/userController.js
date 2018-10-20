8e04a3ee210fdace264d373cf49bb2dc72302339
Paulus Kurnianto <paulus.kurnianto@gameloft.com>
2018-10-20 21:40:54 +0700

    add validation on user create

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,24 +1,39 @@
+const { check, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 var User = require('../model/userModel');
 
 exports.createUser = function (req, res, next) {
-    console.log('call from user/create');
+    check('username', 'Username is required').notEmpty();
+    check('password', 'Password is required').notEmpty();
+    check('password', 'Password character minimum must be 6').isLength({ min: 5 });
+    check('passwordConf', 'Password Confirmation is required').notEmpty();
+    check('passowrdConf', 'Password Confirmation do not match').equals(req.body.password);
 
-    let newUser = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
+    sanitizeBody('username').trim().escape();
+    sanitizeBody('password').trim().escape();
 
-    newUser.password = newUser.getHash(req.body.password);
-
-    newUser.save(function (err) {
-        if (err) {
-            return next(err)
-        };
-        res.json({ user: newUser });
-    });
+    const errors = validationResult(req);
+    if (errors) {
+        res.status(400).json({message: errors});
+    } else {
+        let newUser = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+    
+        newUser.password = newUser.getHash(req.body.password);
+    
+        newUser.save(function (err) {
+            if (err) {
+                return next(err)
+            };
+            res.json({ user: newUser });
+        });
+    }    
 };
 
-exports.get_createUser = function(req, res, next) {
+exports.get_createUser = function(req, res, next) { // buat opo
     console.log('test oyy');
 
     User.find().exec(function(err, dbSuccess) {
--- app/routes/users.js
+++ app/routes/users.js
@@ -4,6 +4,6 @@ var router = express.Router();
 var userController = require('../controller/userController');
 
 router.post('/user/create', userController.createUser);
-router.get('/user/create', userController.get_createUser);
+router.get('/user/create', userController.get_createUser); //buat opo
 
 module.exports = router;
