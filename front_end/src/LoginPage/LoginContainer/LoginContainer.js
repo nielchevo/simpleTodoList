112935c836e34371544ef0a742fdbafaa4608514
Paulus Kurnianto <paulus.kurnianto@gameloft.com>
2018-11-06 15:30:30 +0700

    modifying login form, is okay now

--- front_end/src/LoginPage/LoginContainer/LoginContainer.js
+++ front_end/src/LoginPage/LoginContainer/LoginContainer.js
@@ -1,5 +1,7 @@
 ﻿import React, { Component } from 'react';
 import axios from 'axios';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 class LoginContainer extends Component {
     constructor(props) {
@@ -12,22 +14,21 @@ class LoginContainer extends Component {
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleClearForm = this.handleClearForm.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+        // debug
+        this.debugLogState = this.debugLogState.bind(this);
     }
 
     async handleSubmit(e) {
         e.preventDefault();
 
-        console.log('submiting login');
+        //console.log(this.state.username);
+        //console.log(this.state.password);
         await axios.post('http://localhost:5000/user/login', {
             username: this.state.username,
             password: this.state.password
         }
-        /*, {
-                headers: {
-                    //'Authorization': `Token ${still_none}`
-                    'Authorization': `Token hai`
-                }
-            }*/
         );
     }
 
@@ -35,34 +36,48 @@ class LoginContainer extends Component {
 
     }
 
-    //render() {
-    //    return (
-    //        <div className="container h-100 identifyProb">
-    //            <div className="row h-100 justify-content-center align-items-center">
-    //                <form className="col-12">
-    //                    <div className="form-group">
-    //                        <label htmlFor="inputUsername">username</label>
-    //                        <input type="text" className="form-control" id="username" placeholder="username" />
-    //                    </div>
-    //                    <div className="form-group">
-    //                        <label htmlFor="inputPassword">password</label>
-    //                        <input type="password" className="form-control" id="password" placeholder="password" />
-    //                    </div>
-    //                    <div className="form-group">
-    //                        <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onClick={this.handleSubmit}>Sign in</button>
-    //                    </div>                        
-    //                </form>
-    //            </div>
-    //        </div>
-    //    )
-    //}
+    handleChange = (e) => {
+        let newState = {}
+        newState[e.target.name] = e.target.value
+        this.setState(newState)
+    }
+
+    debugLogState = (e) => {
+        e.preventDefault();
+
+        console.log(this.state);
+    }
 
     render() {
         return (
             <form className="container" onSubmit={this.handleSubmit}>
-                <Input />
-                <Input />
-                <Button />
+                <Input
+                    type={"text"}
+                    title={"Username"}
+                    name={"username"}
+                    value={this.state.username}
+                    placeholder={"enter username"}
+                    handleChange={this.handleChange}
+                />
+                <Input
+                    type={"password"}
+                    title={"Password"}
+                    name={"password"}
+                    value={this.state.password}
+                    placeholder={"enter password"}
+                    handleChange={this.handleChange}
+                />
+                <Button
+                    action={this.handleSubmit}
+                    type={"primary"}
+                    title={"Login"}
+                />
+                {/*{" "}
+                <Button
+                    action={this.debugLogState}
+                    type={"secondary"}
+                    title={"DEBUG"}
+                />*/}
             </form>    
         );        
     }
--- front_end/src/components/Input.jsx
+++ front_end/src/components/Input.jsx
@@ -3,9 +3,9 @@
 const Input = (props) => {
     return (
         <div className="form-group">
-            <label for={props.name} className="form-label">{props.title}</label>
+            <label htmlFor={props.name} className="form-label">{props.title}</label>
             <input
-                className="form-input"
+                className="form-control"
                 id={props.name}
                 name={props.name}
                 type={props.type}
