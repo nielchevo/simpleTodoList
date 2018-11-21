2d30e8898ce75c18ed1c8d64534d425238c98e45
nielchevo <nataniel.gunawan@gameloft.com>
2018-11-21 13:05:30 +0700

    +add dummyData -fix navbar warning 'anchor-is-valid'

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
 const NavBar = (props) => {
+    
+    const onClickHandle = (e) => {
+        console.log('onClickHandle() event');
+    }
+
     return(
         <nav className="navbar navbar-expand-sm navbar-right bg-light justify-content-end">
             <ul className="navbar-nav">
                 {/* TODO: convert nav-item to component so it can handle the onclick 'active' 
                     https://stackoverflow.com/questions/22461129/switch-class-on-tabs-with-react-js*/}
                 <li className="nav-item active">
-                    <a className="nav-link" href="#">Home</a>
+                    <a className="nav-link" href="#section" onClick={onClickHandle}>Home</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
+                    <a className="nav-link" href="#section" onClick={onClickHandle}>About</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
+                    <a className="nav-link" href="#section"  onClick={onClickHandle}>Login</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Register</a>
+                    <a className="nav-link" href="#section" onClick={onClickHandle}>Register</a>
                 </li>
             </ul>
         </nav>
