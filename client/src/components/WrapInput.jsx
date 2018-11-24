76f5a8ba3949a1f54b9fd650b6e9ff1cdd343491
Paulus Kurnianto <paulus.kurnianto@gameloft.com>
2018-11-24 22:24:24 +0700

    add new wrapper for input

---
+++ client/src/components/WrapInput.jsx
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+
+// Component for input field without label
+
+class WrapInput extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleChange = (e) => {
+        const { name, onChange } = this.props;
+        const newValue = e.target.value;
+        onChange(name, newValue);
+    }
+
+    render() {
+        const { value } = this.props;
+        return (
+            <input
+                className="form-control"
+                id={props.id}
+                name={props.name}
+                type={props.type}
+                value={value}
+                onChange={this.handleChange}
+                placeholder={props.placeholder}
+            />  
+        );
+    }
+}
+
+export default WrapInput
