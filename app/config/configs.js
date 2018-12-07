d3b763f48f5995df3987f08c1ec05463b8058298
nielchevo <nataniel.gunawan@gameloft.com>
2018-12-07 13:22:21 +0700

    ! change Auth token lifespan from '1h' to '1w' for testing axios call API purpose ,

--- app/config/configs.js
+++ app/config/configs.js
@@ -1,6 +1,6 @@
 module.exports = {
     AccessSecret: 'secretisrahasia',        // API access token
-    AccessLifetime: '1h',
+    AccessLifetime: '1w',
     RefreshSecret: 'RefreshUntukKeamanan',  // Auth session token
     RefreshLifetime: '1w'
 };
