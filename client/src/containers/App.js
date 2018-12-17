e9c8a40029e9418687876860cc8ce56ac744d924
thegray <pawz02@gmail.com>
2018-12-18 01:54:06 +0700

    add not found page for routes with no handler

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import Login from './Login';
 import TodoPage from './TodoPage';
+import NotFoundPage from './NotFoundPage';
 import SecuredRoute from '../components/SecuredRoute';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <SecuredRoute exact path='/' component={HomePage} />
-                <Route path='/login' component={Login} />
-                <Route path='/todo' component={TodoPage} />
-
+                <Switch>
+                    <SecuredRoute exact path='/' component={HomePage} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/todo' component={TodoPage} />
+                    <Route path="*" component={NotFoundPage} />
+                </Switch>
             </div>
         );
     }
