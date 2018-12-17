0d92cc14246c1f83751622a731735bce9c187935
Bangun Kurnianto <pawz02@gmail.com>
2018-12-17 11:30:05 +0700

    add get header auth function helper

--- client/src/actions/TodoActions.js
+++ client/src/actions/TodoActions.js
@@ -1,13 +1,13 @@
-
-import * as Types from '../constants/ActionTypes';
 import Axios from 'axios';
+import * as Types from '../constants/ActionTypes';
+import { authHeader } from '../modules/authHelper';
 
 // dummy Auth Token , if this fail set token lifespan to '1w' in back-end configs.js
 const tokenUser = ''; 
 
-const header_config = {
-     headers: { Authorization: 'Token ' + tokenUser }
-}
+//const header_config = {
+//     headers: { Authorization: 'Token ' + tokenUser }
+//}
 
 export const addTodoAction = (newtodo) => ({
     type: Types.ADD_TODO,
@@ -16,7 +16,7 @@ export const addTodoAction = (newtodo) => ({
 
 export const addTodo = (listItem) => {
    return (dispatch) => {
-      return Axios.post('http://localhost:5000/todo/create', listItem, header_config)
+       return Axios.post('http://localhost:5000/todo/create', listItem, authHeader())
          .then(response => {
             dispatch(addTodoAction(response.data));
          })
@@ -46,7 +46,7 @@ export const fetchTodoAction = (todos) => ({
 
 export const fetchTodo = (username) => {
     return (dispatch) => {
-        return Axios.get('http://localhost:5000/todos', header_config)
+        return Axios.get('http://localhost:5000/todos', authHeader())
             .then(response => {
                 // success, pass to todoReducer
                 dispatch(fetchTodoAction(response.data));
