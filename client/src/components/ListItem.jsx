6480260183f7bfb70ee55dbe8e40e4bfd4f7bbbd
Bangun Kurnianto <pawz02@gmail.com>
2018-12-20 15:05:10 +0700

    fix crash in displaying list inside card, strange case where list stored is null

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -2,38 +2,36 @@ import React from 'react';
 
 const ListItem = ({cardID, itemList, deleteItemList, itemIsCompleted}) => {
     // note: cardID act as key to known where itemlist we want to modify.
-
-    const items = itemList.length ? (
-      itemList.map((item) => {
-         return (
-            <li 
-               className="list-group-item list-group-item-action" 
-               key={item._id} 
-               > 
-                  {item.content}
-                  <div className="btn-wrapper">
-                     <button className="btn btn-primary" onClick={ ()=> deleteItemList(item._id, cardID) }> 
-                        Delete !
-                     </button>
-                     <button className="btn btn-secondary" onClick={ () => itemIsCompleted(item._id, cardID) }>
-                        Complete
-                     </button>
-                  </div>
-            </li>
-         )
-      })
+    const items = (itemList && itemList.length) ? (
+        itemList.map((item) => {
+            return (
+                <li
+                    className="list-group-item list-group-item-action"
+                    key={item._id}
+                >
+                    {item.content}
+                    <div className="btn-wrapper">
+                        <button className="btn btn-primary" onClick={() => deleteItemList(item._id, cardID)}>
+                            Delete !
+                    </button>
+                        <button className="btn btn-secondary" onClick={() => itemIsCompleted(item._id, cardID)}>
+                            Complete
+                    </button>
+                    </div>
+                </li>
+            )
+        })
     ) : (
-         <li className="list-group-item list-group-item-action">
-            No Todo, please add one 
-         </li>
-         )
-         
+        <li className="list-group-item list-group-item-action">
+                No Todo, please add one
+        </li>
+    )
 
-	return(
-      <div className="list-group">
-         {items}
-      </div>
-	)
+    return (
+        <div className="list-group">
+            {items}
+        </div>
+    )
 }
 
 export default ListItem;
