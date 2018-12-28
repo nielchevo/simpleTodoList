26f655a06e571c39f467dae28fbe7318c0d815bd
Bangun Kurnianto <pawz02@gmail.com>
2018-12-28 11:33:12 +0700

    add testing remote db, and it works!

--- app/config/dbConnect.js
+++ app/config/dbConnect.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
+//var mongoURL = 'pawz:simpletodolist1@ds151809.mlab.com:51809';
 var mongoURL = 'localhost';
+//var dbName   = 'simpletodolist';
 var dbName   = 'simpleTodoList';
 mongoose.connect('mongodb://'+ mongoURL +'/'+ dbName, {useNewUrlParser: true} );
 mongoose.Promise = global.Promise;
